dd4ffa26e9d0efb11059e0c32d49b8d3e8da18d3
KhayakaziM <mqhkay@gmail.com>
2021-07-18 18:40:33 +0200

    Update ResourceNotFoundException.java

--- store/src/main/java/com/triggerise/store/exception/ResourceNotFoundException.java
+++ store/src/main/java/com/triggerise/store/exception/ResourceNotFoundException.java
@@ -5,8 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
-
-    private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
 
     public ResourceNotFoundException(String message) {
         super(message);
@@ -15,4 +14,5 @@ public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message, Throwable throwable) {
         super(message, throwable);
     }
+  
 }
